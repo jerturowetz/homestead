91872345c4733fa61fb9dbe223050b377133e02a
Jeremy Turowetz <j.turowetz@gmail.com>
2017-10-06 19:41:48 -0400

    add working code for aws copy

--- scripts/aws-s3-copy.sh
+++ scripts/aws-s3-copy.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-# Clone Jer's plugins repo
-if [[ ! -f "${SITE_DIR}/wp-content/plugins/index.php" ]]; then
-    echo "Cloning Jer's plugins repo"
-    cd ${SITE_DIR}/wp-content
-    git clone git@github.com:JerTurowetz/.wp-plugins.git plugins
-else
-    echo "Updating Jer's plugins repo"
-    cd ${SITE_DIR}/wp-content/plugins
-    git pull
-fi
+#!/usr/bin/env bash
 
 # export AWS_PROFILE=standard
 # aws_access_key_id=`curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep AccessKeyId | cut -d':' -f2 | sed 's/[^0-9A-Z]*//g'`
@@ -24,8 +13,3 @@ fi
 # export AWS_PROFILE=standard
 # aws s3 sync s3://largefs-standardpro/standardpro/wp-content/uploads/ "${SITE_PATH}/wp-content/uploads/"
 # aws s3 cp s3://largefs-standardpro/standardpro/wp-content/uploads/ "${SITE_PATH}/wp-content/uploads/" --recursive
-
-# should probably only do this once...
-# echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-
-
