a94cde67a56d0472803d3c8202a920df86e0755b
Jeremy Turowetz <j.turowetz@gmail.com>
2017-10-10 15:57:13 -0400

    add more vars and add url update script

--- scripts/wordpress-core-update.sh
+++ scripts/wordpress-core-update.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-SITE_DIR=${1}
-WP_FOLDER_NAME="wordpress"
+SITE_DIR="${1}"
+SITE_URL="${2}"
+WP_FOLDER_NAME="${3}"
 
 # Update WP version if possible
 echo "update wordpress core to latest version"
@@ -11,3 +12,10 @@ wp core update --version="latest" --path="${WP_FOLDER_NAME}/"
 # flush permalinks
 echo "Flushing permalinks"
 wp rewrite flush
+
+# Set Home URLS
+echo "Reset home urls"
+cd "${SITE_DIR}"
+echo "${SITE_URL}/${WP_FOLDER_NAME}"
+wp option update home "http://${SITE_URL}"
+wp option update siteurl "http://${SITE_URL}/${WP_FOLDER_NAME}"
