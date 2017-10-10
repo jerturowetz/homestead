b9f1618d717e01668298c6c5e77da1302cebaf2a
Jeremy Turowetz <j.turowetz@gmail.com>
2017-10-10 15:57:38 -0400

    add lines to create folder if not exists

--- scripts/wpengine-copy-uploads-folder.sh
+++ scripts/wpengine-copy-uploads-folder.sh
@@ -4,10 +4,17 @@ SFTP_SERVER="${2}"
 SFTP_USER="${3}"
 SFTP_PASS="${4}"
 
+LOCAL_UPLOADS_FOLDER="wp-content/test"
+
 # Install ssh pass
 echo "installing sshpass"
 sudo apt install sshpass
 
 # Copy contents of sftp server /wp-content/uploads
+if [[ ! -d "${SITE_DIR}/${LOCAL_UPLOADS_FOLDER}" ]];
+then
+    mkdir "${SITE_DIR}/${LOCAL_UPLOADS_FOLDER}"
+fi
+
 echo "copying contents of WP Engine wp-content/uploads folder, this can take a minute"
-sshpass -p "${SFTP_PASS}" scp -P 2222 -o StrictHostKeyChecking=no -r "${SFTP_USER}@${SFTP_SERVER}:/wp-content/uploads" "${SITE_DIR}/wp-content/uploads"
+sshpass -p "${SFTP_PASS}" scp -P 2222 -o StrictHostKeyChecking=no -r "${SFTP_USER}@${SFTP_SERVER}:/wp-content/uploads/." "${SITE_DIR}/${LOCAL_UPLOADS_FOLDER}/"
