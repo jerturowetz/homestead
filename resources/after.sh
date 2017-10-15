44da08f6aa24284591d70047fe365db8eb931dd9
Jeremy Turowetz <j.turowetz@gmail.com>
2017-10-15 17:28:50 -0400

    hide ldap stuff from after.sh

--- resources/after.sh
+++ resources/after.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update apt-get
-echo "Updating apt-get"
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update
+#echo "Updating apt-get"
+#sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update
 
 # Install phpX.X-ldap
-echo "Installing phpX.X-ldap (matches current php version)"
-PHPVersion=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,2,3);
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install php${PHPVersion}-ldap
+#echo "Installing phpX.X-ldap (matches current php version)"
+#PHPVersion=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,2,3);
+#sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install php${PHPVersion}-ldap
