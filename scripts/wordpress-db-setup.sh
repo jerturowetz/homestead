c3dc6fde0d2efe9116418bc56122b79e49f2d274
Jeremy Turowetz <j.turowetz@gmail.com>
2017-12-11 19:37:32 -0500

    remove inline local db user name and pass (uneeded)

--- scripts/wordpress-db-setup.sh
+++ scripts/wordpress-db-setup.sh
@@ -2,8 +2,11 @@
 
 SITE_DIR="${1}"
 LOCAL_DB_NAME="${2}"
-LOCAL_DB_USER="homestead"
-LOCAL_DB_PASS="secret"
+
+# # Homestead already copies .my.cnf to the /home/vagrant folder so we shouldn't need the following
+# LOCAL_DB_USER="homestead"
+# LOCAL_DB_PASS="secret"
+# # I've also removed inline user & pass from the scripts below
 
 HOMESTEAD_SITE_URL="${3}"
 
@@ -15,7 +18,8 @@ REMOTE_DB_NAME="wp_${WP_ENGINE_INSTALL_NAME}"
 REMOTE_DB_USER="${WP_ENGINE_INSTALL_NAME}"
 REMOTE_DB_PASS="${6}"
 
-echo "wordpress-db-setup.sh >> maybe write a function to check if network connection is available? the next item errors out when not connected to the interten"
+
+echo "Write a function to check if network connection is available as the mysql remote check returns an error (which i guess is fine, just ugly)"
 if (( $# == 6 )) && mysql -u "${REMOTE_DB_USER}" -p"${REMOTE_DB_PASS}" -h "${REMOTE_DB_URL}" "${REMOTE_DB_NAME}" -e ";" ;
 then
     echo "dumpig database from prod to local site folder"
@@ -34,14 +38,14 @@ fi
 if [ -f "dump.sql" ];
 then
     echo "found dump file, importing and erasing"
-    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} drop ${LOCAL_DB_NAME} -f
-    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} create ${LOCAL_DB_NAME}
-    mysql -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} ${LOCAL_DB_NAME} < dump.sql
+    mysqladmin drop ${LOCAL_DB_NAME} -f
+    mysqladmin create ${LOCAL_DB_NAME}
+    mysql ${LOCAL_DB_NAME} < dump.sql
     rm dump.sql
 else
     echo "install wp db from scratch - migh cause errors if db doesn't exist? dropping is important if db doesn exist"
     cd "${SITE_DIR}"
-    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} drop ${LOCAL_DB_NAME} -f
-    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} create ${LOCAL_DB_NAME}
+    mysqladmin drop ${LOCAL_DB_NAME} -f
+    mysqladmin create ${LOCAL_DB_NAME}
     wp core install --url="${SITE_URL}" --title=Homestead --admin_user=homestead --admin_password=secret --admin_email=tempemail@email.com --skip-email --path="${WP_FOLDER_NAME}/"
 fi
