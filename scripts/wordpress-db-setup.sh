0d33da23b1944e75469dc7c76d3a200af50eb109
Jeremy Turowetz <j.turowetz@gmail.com>
2017-12-11 19:23:31 -0500

    update wordpress-db-setup scripts

--- scripts/wordpress-db-setup.sh
+++ scripts/wordpress-db-setup.sh
@@ -15,24 +15,21 @@ REMOTE_DB_NAME="wp_${WP_ENGINE_INSTALL_NAME}"
 REMOTE_DB_USER="${WP_ENGINE_INSTALL_NAME}"
 REMOTE_DB_PASS="${6}"
 
-while true;
-do
-    if (( $# == 6 )) && mysql -u "${REMOTE_DB_USER}" -p"${REMOTE_DB_PASS}" -h "${REMOTE_DB_URL}" "${REMOTE_DB_NAME}" -e ";" ;
-    then
-        echo "dumpig database from prod to local site folder"
-        mysqldump -u ${REMOTE_DB_USER} -p${REMOTE_DB_PASS} -h ${REMOTE_DB_URL} ${REMOTE_DB_NAME} > "${SITE_DIR}/mysql.sql"
-    fi
+echo "wordpress-db-setup.sh >> maybe write a function to check if network connection is available? the next item errors out when not connected to the interten"
+if (( $# == 6 )) && mysql -u "${REMOTE_DB_USER}" -p"${REMOTE_DB_PASS}" -h "${REMOTE_DB_URL}" "${REMOTE_DB_NAME}" -e ";" ;
+then
+    echo "dumpig database from prod to local site folder"
+    mysqldump -u ${REMOTE_DB_USER} -p${REMOTE_DB_PASS} -h ${REMOTE_DB_URL} ${REMOTE_DB_NAME} > "${SITE_DIR}/mysql.sql"
+fi
 
-    if [ -f "${SITE_DIR}/mysql.sql" ];
-    then
-        echo "Copying mysql.sql found in site folder to dump file"
-        cp "${SITE_DIR}/mysql.sql" dump.sql
-        break
-    fi
 
+if [ -f "${SITE_DIR}/mysql.sql" ];
+then
+    echo "Copying mysql.sql found in site folder to dump file"
+    cp "${SITE_DIR}/mysql.sql" dump.sql
     break
+fi
 
-done
 
 if [ -f "dump.sql" ];
 then
@@ -42,7 +39,9 @@ then
     mysql -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} ${LOCAL_DB_NAME} < dump.sql
     rm dump.sql
 else
-    echo "install wp from scratch"
-    cd "${SITE_DIR}"    
+    echo "install wp db from scratch - migh cause errors if db doesn't exist? dropping is important if db doesn exist"
+    cd "${SITE_DIR}"
+    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} drop ${LOCAL_DB_NAME} -f
+    mysqladmin -u ${LOCAL_DB_USER} -p${LOCAL_DB_PASS} create ${LOCAL_DB_NAME}
     wp core install --url="${SITE_URL}" --title=Homestead --admin_user=homestead --admin_password=secret --admin_email=tempemail@email.com --skip-email --path="${WP_FOLDER_NAME}/"
 fi
